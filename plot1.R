e26761ac86d8a64fb9bf517c00b17dbd30f5239f
Arjun <arjunb18@outlook.com>
2015-07-07 15:39:15 +0530

    Course Project 1 of the Exploratory Data Analysis course on Coursera

---
+++ plot1.R
@@ -0,0 +1,10 @@
+
+setwd("~/R")
+rm(list=ls())
+data1<-read.csv("household_power_consumption.txt",sep=";",header = T,comment.char  = "",stringsAsFactors = FALSE)
+data1$Date <- as.Date(data1$Date, format="%d/%m/%Y")
+data<- subset(data1, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+rm(data1)                             
+hist(data$Global_active_power, main="Global Active Power", xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+dev.copy(png, file="plot1.png", height=480, width=480)
+dev.off()
